2ab171723d9ddf0c8c92773c20f3f2de597250a0
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-03-20 20:23:00 +0100

    Basic useEffect construction
    Implementing useEffect functionality in react

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-const MovieCard = () => {
-  return <div className="film-list" />;
+const MovieCard = props => {
+  return (
+    <li className="movie-card" key={props.id}>
+      <h2>Film title: {props.title}</h2>
+      <p>{props.description}</p>
+      <p>Director: {props.director}</p>
+      <p>
+        Release date:{" "}
+        <small className="text-warning">{props.release_date}</small>
+      </p>
+    </li>
+  );
 };
 export default MovieCard;
