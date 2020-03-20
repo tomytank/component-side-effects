6f22d6cfc3d892c392df8a48235ca9cfefe1e90b
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-03-20 20:53:45 +0100

    Variable name changes, added logic
    Added appropriate variable names to represent their functions and inserted assessment logic to instigate on-screen changes.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import logo from "./assets/logo.png";
 import "./styles.css";
 
 function App() {
-  const [state, setState] = useState(0);
-  const [otherState, setOtherState] = useState("dan");
+  const [count, setCount] = useState(0);
+  const [countLevel, setCountLevel] = useState("Low");
 
   useEffect(() => {
     console.log("Component mounted!");
@@ -14,27 +14,38 @@ function App() {
 
   useEffect(() => {
     console.log("State count changed!");
-  }, [state]);
+    //1-5 is low
+    //5-15 is medium
+    //15+ is high
+    if (count < 6) {
+      //do nothing
+    } else if (count > 5 && count < 15) {
+      setCountLevel("Medium");
+    } else {
+      setCountLevel("High");
+    }
+  }, [count]);
 
   useEffect(() => {
     console.log("State Other Changed");
-  }, [otherState]);
+  }, [countLevel]);
 
-  useEffect(() => {
-    console.log("ANY state changes");
-  }, [state, otherState]);
+  // useEffect(() => {
+  //   console.log("ANY state changes");
+  // }, [countLevel, count]);
 
   return (
     <div className="App">
       <div className="logo_container">
         <img className="logo" src={logo} alt="sudio ghibli official logo" />
         <h1>API</h1>
+        <h2>The count is: {countLevel}</h2>
         <button
           onClick={() => {
-            setState(state + 1);
+            setCount(count + 1);
           }}
         >
-          {state} times
+          {count} times
         </button>
       </div>
     </div>
