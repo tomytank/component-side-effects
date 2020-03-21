fbc94a45e58030ec6bdf01ea0865ec6386df89a4
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-03-21 23:24:59 +0100

    Code tidy up
    Remove old, unnecessary code.

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   }, [count]);
 
   useEffect(() => {
-    console.log("State Other Changed");
+    console.log("State countLevel Changed");
   }, [countLevel]);
 
   // useEffect(() => {
@@ -53,54 +53,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [countLevel, setCountLevel] = useState("Low");
-
-//   // useEffect(() => {
-//   //   console.log("Component Mounted!")
-//   // }, []);
-
-//   useEffect(() => {
-//     console.log("State count changed");
-//     // 1- 5 is Low,
-//     // 5- 15 is Medium
-//     // 15+ is high
-//     if (count < 6) {
-//       // Do nothing
-//     } else if (count > 5 && count < 16) {
-//       setCountLevel("Medium");
-//     } else {
-//       setCountLevel("High");
-//     }
-//   }, [count]);
-
-//   // useEffect(() => {
-//   //   console.log("State Other changed")
-//   // }, [countLevel])
-
-//   // useEffect(() => {
-//   //   console.log("Any state has changed")
-//   // }, [countLevel, count])
-
-//   return (
-//     <div className="App">
-//       <div className="logo_container">
-//         <img className="logo" src={logo} alt="studio ghibli official logo" />
-//         <h1>API</h1>
-//         <h2>The count is: {countLevel}</h2>
-//         <button
-//           onClick={() => {
-//             setCount(count + 1);
-//           }}
-//         >
-//           {count} times
-//         </button>
-//       </div>
-//       <MoviesList />
-//     </div>
-//   );
-// }
-
-// export default App;
